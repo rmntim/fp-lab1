e88447b3c76cca17917769634e082fa42a0a448e
Roman Timoshkin <rmntim@icloud.com>
2025-09-23 14:56:41 +0300

    refactor: reimplement 16 problem with bigints

--- lib/euler_problems.ml
+++ lib/euler_problems.ml
@@ -41,9 +41,9 @@ module Euler14 = struct
     generate limit |> filter_numbers limit |> reduce_numbers (1, 1) |> fst
 
   let solve_with_map limit =
-    Array.init (limit - 1) (fun i -> i + 1)
-    |> Array.map (fun n -> (n, collatz_length_tail n))
-    |> Array.fold_left
+    List.init (limit - 1) (fun i -> i + 1)
+    |> List.map (fun n -> (n, collatz_length_tail n))
+    |> List.fold_left
          (fun (best, best_len) (n, len) ->
            if len > best_len then (n, len) else (best, best_len))
          (1, 1)
@@ -71,18 +71,47 @@ module Euler14 = struct
 end
 
 module Euler16 = struct
-  open Z
+  let explode_string s = List.init (String.length s) (String.get s)
+  let int_from_char c = int_of_char c - int_of_char '0'
 
   let solve_monolithic_tail base power =
     let rec aux acc = function
       | [] -> acc
-      | d :: rest -> aux (acc + int_of_string d) rest
+      | d :: rest -> aux (acc + int_from_char d) rest
     in
-    power_int_positive_int base power |> string_of_big_int |> aux 0
+    Z.pow (Z.of_int base) power |> Z.to_string |> explode_string |> aux 0
 
-  let solve_monolithic_recursive base power = base + power
-  let solve_modular base power = base + power
-  let solve_with_map base power = base + power
-  let solve_with_loops base power = base + power
-  let solve_with_seq base power = base + power
+  let solve_monolithic_recursive base power =
+    let rec aux = function
+      | [] -> 0
+      | d :: rest -> int_from_char d + aux rest
+    in
+    Z.pow (Z.of_int base) power |> Z.to_string |> explode_string |> aux
+
+  let solve_modular base power =
+    let generate base power =
+      Z.pow (Z.of_int base) power |> Z.to_string |> explode_string
+    in
+    let filter = List.filter (( <> ) '0') in
+    let reduce = List.fold_left (fun acc c -> acc + int_from_char c) 0 in
+    generate base power |> filter |> reduce
+
+  let solve_with_map base power =
+    Z.pow (Z.of_int base) power
+    |> Z.to_string |> explode_string |> List.map int_from_char
+    |> List.fold_left ( + ) 0
+
+  let solve_with_loops base power =
+    let acc = ref 0 in
+    let numbers = Z.pow (Z.of_int base) power |> Z.to_string in
+    for n = 0 to String.length numbers - 1 do
+      let number = int_from_char numbers.[n] in
+      acc := !acc + number
+    done;
+    !acc
+
+  let solve_with_seq base power =
+    Z.pow (Z.of_int base) power
+    |> Z.to_string |> String.to_seq |> Seq.map int_from_char
+    |> Seq.fold_left ( + ) 0
 end
